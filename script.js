e0ef3c1c97bcb37af730577790b0f06d38644115
Kenzo <kenzo.delacruz03@gmail.com>
2023-07-12 09:22:36 +0800

    Added the grids

--- script.js
+++ script.js
@@ -1,2 +1,27 @@
-let div = document.getElementById('container')
+const container = document.querySelector('#box');
 
+const div = document.createElement('div');
+
+
+
+
+function createDiv(num) {
+    for (let i = 0; i < num; i++)
+    {
+        const div = document.createElement('div');
+        div.setAttribute('id', 'grid');
+        div.style.border = ".5px solid black";
+        container.appendChild(div);
+    }
+}
+
+
+createDiv(256);
+
+const grid = document.querySelectorAll("[id='grid']");
+
+document.getElementById('grid').addEventListener('mouseover', changeColor);
+
+function changeColor() {
+    frid.style.backgroundColor = 'black';
+}
